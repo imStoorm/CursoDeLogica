bca91b4dceb1d8c23a383111160ada2c162c42db
imStoorm <imstoormdev@proton.me>
2023-10-07 12:54:57 -0300

    Python added

---
+++ python/getfatorial.py
@@ -0,0 +1,7 @@
+NUMERO=5
+RES=NUMERO
+I=NUMERO-1
+while (I > 0):
+    RES=RES*I
+    I=I-1
+print(RES)
---
+++ python/somaridades.py
@@ -0,0 +1,5 @@
+IDADES = [11, 12, 13, 15, 19, 32]
+RES = 0
+for idade in IDADES:
+    RES=RES+idade
+print(RES)
